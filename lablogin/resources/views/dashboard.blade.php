7a5873f26716ad041a08ef31c29e8edd9f04142b
Melchizedek Recalde <melchizedek.recalde@studentambassadors.com>
2021-01-15 14:17:45 +0800

    Small addition to our page content.

--- lablogin/resources/views/dashboard.blade.php
+++ lablogin/resources/views/dashboard.blade.php
@@ -84,6 +84,31 @@
             </div>
 </div>
 
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="105773694824940"
+  theme_color="#0A7CFF">
+      </div>
+      
 <script type="text/javascript">
         var analytics = <?php echo $status; ?>
 
